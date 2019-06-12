2af29fdd5cc35e4c2f6383a402d3b42a933c1326
Brittan Fairchild <brittanfairchild@gmail.com>
2019-06-12 05:08:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function sayHiToGrandma(string) {
   var cant = "I can't hear you!";
   var yes = "YES INDEED!";
   var love = "I love you, too.";
-  if string.toLowerCase(string) === string;
+ 
+  if (function whisper(string) === string);
+    return cant
     
   
   
